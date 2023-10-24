3a005a73ea316ceecfaebc9f0607100d514ee02b
tempa <tempa@DESKTOP-08JB4RM.Home>
2023-10-24 14:39:14 +0100

    MYSQL Database connection

---
+++ src/neaproject/NEAProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neaproject;
+
+/**
+ *
+ * @author amakod
+ */
+public class NEAProject {
+
+    
+    public static void main(String[] args) {
+      
+        SignIn log = new SignIn();
+        log.setVisible(true);
+        
+    }
+    
+}
